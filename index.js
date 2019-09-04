e7e67ff28f23b2cffa049459bd942c677c390850
Lohitaksh Trehan <lohitaksh.trehan@iongroup.com>
2019-09-04 14:12:22 +0530

    first commit
    Signed-off-by: Lohitaksh Trehan <lohitaksh.trehan@iongroup.com>

---
+++ index.js
@@ -0,0 +1,1 @@
+//Build objects here
---
+++ models/studentModel.js
@@ -0,0 +1,18 @@
+export class StudentModel {
+    constructor(name, rollNumber) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    setName(name){
+        this.name = name;
+    }
+
+    getRollNumber(){
+        return this.getRollNumber;
+    }
+}
